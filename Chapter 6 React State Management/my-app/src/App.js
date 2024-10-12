53977d02230f4c068cf4e60d53743c08bfe6d244
Jay <eng22cse001@gmail.com>
2024-10-12 19:54:26 +0530

    before taking break

--- Chapter 6 React State Management/my-app/src/App.js
+++ Chapter 6 React State Management/my-app/src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import StarRating from './components/StarRating';
-import { FaStar } from "react-icons/fa";
+
 import React, { useState } from "react";
+import colorData from "./data/color-data.json";
+import ColorList from './components/ColoList';
+
+// export default function App() {
+//   return (
+//     <StarRating style={{ backgroundColor: "lightblue"}}
+//     onDoubleClick={e => alert("double click")}
+//     />
+//   )
+// }
 
 export default function App() {
-  return (
-    <StarRating style={{ backgroundColor: "lightblue"}}
-    onDoubleClick={e => alert("double click")}
-    />
-  )
+  const [colors] = useState(colorData);
+  return <ColorList colors={colors} />;
 }
+
+
--- Chapter 6 React State Management/my-app/src/components/StarRating.js
+++ Chapter 6 React State Management/my-app/src/components/StarRating.js
@@ -16,7 +16,7 @@
 
 
 import { FaStar } from "react-icons/fa";
-import React, { useState } from "react";
+
 
 const createArray = length => [...Array(length)];
 
@@ -24,21 +24,33 @@ const Star = ( { selected = false, onSelect = f => f }) => (
   <FaStar color={selected ? "red" : "grey"} onClick={ onSelect } />
 )
 
-export default function StarRating({ style = {}, totalStars = 5}) {
-  const [selectedStars, setSelectedStars ] = useState(0);
+export default function StarRating({ totalStars = 5 , selectedStars = 0}) {
+  
   return (
-    <div style={{ padding: "5px", ...style}} >
-    {createArray(totalStars).map((n, i) => (
-      <Star 
-      key={i}
-      selected = {selectedStars > i}
-      onSelect = {() => setSelectedStars(i+1)} />
-      
+    <>
+    {createArray(totalStars).map((n,i) => (
+        <Star
+        key={i}
+        selected={selectedStars > i}
+        />
     ))}
     <p>
-      {selectedStars} of {totalStars} stars
+        {selectedStars} of {totalStars} stars
     </p>
-    </div>
+    
+    </>
+    // <div style={{ padding: "5px", ...style}} >
+    // {createArray(totalStars).map((n, i) => (
+    //   <Star 
+    //   key={i}
+    //   selected = {selectedStars > i}
+    //   onSelect = {() => setSelectedStars(i+1)} />
+      
+    // ))}
+    // <p>
+    //   {selectedStars} of {totalStars} stars
+    // </p>
+    // </div>
   );
 
 
