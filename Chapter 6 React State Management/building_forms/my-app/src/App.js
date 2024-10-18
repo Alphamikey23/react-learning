5924e12a8b7333be684bb222856c78902524e2ca
Jay <eng22cse001@gmail.com>
2024-10-18 19:57:00 +0530

    building forms

---
+++ Chapter 6 React State Management/building_forms/my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import AddColorForm from './Components/AddColorForm';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <AddColorForm />
+    </div>
+  );
+}
+
+export default App;
