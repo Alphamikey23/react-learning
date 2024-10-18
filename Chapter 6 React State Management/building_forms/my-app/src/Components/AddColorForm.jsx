82c26241472da404b04cf5537c4d7376c60e9bc8
Jay <eng22cse001@gmail.com>
2024-10-18 21:14:00 +0530

    created custom hooks

--- Chapter 6 React State Management/building_forms/my-app/src/Components/AddColorForm.jsx
+++ Chapter 6 React State Management/building_forms/my-app/src/Components/AddColorForm.jsx
@@ -1,26 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
+import { useInput } from "./hooks";
 
 export default function AddColorForm({ onNewColor = f => f }) {
-    const [title, setTitle] = useState("");
-    const [color, setColor] = useState("#000000");
+    const [titleProps, resetTitle] = useInput("");
+    const [colorProps, resetColor] = useInput("#000000");
 
-    const submit = e => { 
-        e.preventDefault();
+    const submit = event => { 
+        event.preventDefault();
+        onNewColor(titleProps.value, colorProps.value);
+        resetTitle();
+        resetColor();
         
-        onNewColor(title, color);
-        setTitle("");
-        setColor("");
+        
     }
 
     return(
         <form onSubmit={submit}>
             <input 
-            value={title}
-            onChange={event => setTitle(event.target.value)}
-             type="text" placeholder="color title..." required />
+            {...titleProps}
+            type="text" placeholder="color title..." required />
             <input 
-            value={color}
-            onChange = { event => setColor(event.target.value)} type="color" required />
+            {...colorProps} type="color"  required />
             <button>ADD</button>
         </form>
     );
