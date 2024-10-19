468c963f97530b6b2ab2ed1617b898b78970e0b9
Jay <eng22cse001@gmail.com>
2024-10-19 21:45:11 +0530

    placing colors in context modified App.js to work

--- Chapter 6 React State Management/building_forms/my-app/src/App.js
+++ Chapter 6 React State Management/building_forms/my-app/src/App.js
@@ -2,25 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import AddColorForm from './Components/AddColorForm';
 import React, { useState } from "react";
-import colorData from './data/color-data.json'
+
 import ColorList from './Components/ColorList';
-import { v4 } from "uuid";
+
 
 function App() {
-  const [colors, setColors] = useState(colorData);
+  
   return (
     <>
-    <AddColorForm onNewColor={(title, color) => {
-      const newColors = [...colors, {
-        id: v4(),
-        rating: 0,
-        title,
-        color
-      }];
-
-      setColors(newColors);
-    }} />
-    
+    <AddColorForm />
     <ColorList />
     </>
 
