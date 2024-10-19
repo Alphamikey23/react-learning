1df28c5a84e0f797de974bd3cce16cc3f4662f95
Jay <eng22cse001@gmail.com>
2024-10-19 21:39:14 +0530

    placing colors in context

--- Chapter 6 React State Management/building_forms/my-app/src/index.js
+++ Chapter 6 React State Management/building_forms/my-app/src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {  createContext }from 'react';
+import colors from "./color-data";
+import { render } from "react-dom";
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+export const ColorContext = createContext();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ColorContext.Provider value={{ colors}}>
     <App />
+    </ColorContext.Provider>
+  
   </React.StrictMode>
 );
 
